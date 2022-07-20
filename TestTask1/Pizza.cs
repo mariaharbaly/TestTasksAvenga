b320a520bfdb9583fdd242b720c4f5ae2eb299d0
mariaharbaly <garbalyy@gmail.com>
2022-07-21 01:22:59 +0300

    initial commit

---
+++ TestTask1/Pizza.cs
@@ -0,0 +1,6 @@
+﻿namespace TestTask1;
+
+public class Pizza
+{
+    public List<string> Toppings { get; set; }
+}
---
+++ TestTask1/Program.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using TestTask1;
+
+string text = System.IO.File.ReadAllText(@"C:\Users\garba\RiderProjects\TestTasksAvenga\TestTask1\Test task #1 - Pizzas (1) (1).json");
+
+List<Pizza>? items = JsonConvert.DeserializeObject<List<Pizza>>(text);
+
+var result = items.GroupBy(x => string.Join( ", ",  x.Toppings.OrderBy(y => y)))
+    .Select(g => new { g.Key, Count = g.Count() })
+    .OrderByDescending(x => x.Count);
+
+Console.WriteLine("The most popular combinations of toppings: ");
+foreach (var pizza in result.Take(20))
+{
+    Console.WriteLine($"{pizza.Key} - {pizza.Count}");
+}
+
+Console.ReadLine();
