f3c9925c67a5ed97db9b1d10946bbc7d18b1a7e0
mariaharbaly <garbalyy@gmail.com>
2022-07-21 14:20:15 +0300

    refactor code

---
+++ TestTask2/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Refactoring;
+
+public class Product
+{
+    public string ProductName;
+    public decimal Price;
+    public decimal? Weight;
+    public int? Quantity;
+    public string PricingMethod;
+}
--- TestTask2/Program.cs
+++ TestTask2/Program.cs
@@ -1,71 +1,74 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Text;
 
 namespace Refactoring
 {
     class Program
     {
+        private const string PerPound = "PerPound";
+        private const string PerItem = "PerItem";
+        
         static void Main(string[] args)
         {
-            var order = new Tuple<string, List<Product>>("John Doe",
-                new List<Product>
+            var orders = new Dictionary<string, List<Product>>()
+            {
                 {
-                    new Product
+                    "John Doe",
+                    new List<Product>
                     {
-                        ProductName = "Pulled Pork",
-                        Price = 6.99m,
-                        Weight = 0.5m,
-                        PricingMethod = "PerPound",
-                    },
-                    new Product
-                    {
-                        ProductName = "Coke",
-                        Price = 3m,
-                        Quantity = 2,
-                        PricingMethod = "PerItem"
+                        new()
+                        {
+                            ProductName = "Pulled Pork",
+                            Price = 6.99m,
+                            Weight = 0.5m,
+                            PricingMethod = PerPound,
+                        },
+                        new()
+                        {
+                            ProductName = "Coke",
+                            Price = 3m,
+                            Quantity = 2,
+                            PricingMethod = PerItem
+                        }
                     }
                 }
-            );
- 
-            var price = 0m;
-            StringBuilder orderSummary = new StringBuilder("ORDER SUMMARY FOR " + order.Item1 + ": \r\n");
+            };
             
-            foreach (var orderProduct in order.Item2)
+            
+            foreach (var order in orders)
             {
-                var productPrice = 0m;
-                orderSummary.Append(orderProduct.ProductName);
- 
-                if (orderProduct.PricingMethod == "PerPound")
+                var totalPrice = 0m;
+                
+                StringBuilder orderSummary = new StringBuilder($"ORDER SUMMARY FOR {order.Key} \r\n");
+                
+                foreach (var product in order.Value)
                 {
-                    productPrice = (orderProduct.Weight.Value * orderProduct.Price);
-                    price += productPrice;
-                    orderSummary.Append(" $" + productPrice + " (" + orderProduct.Weight + " pounds at $" + orderProduct.Price + " per pound)");
-                }
-                else // Per item
-                {
-                    productPrice = (orderProduct.Quantity.Value * orderProduct.Price);
-                    price += productPrice;
-                    orderSummary.Append(" $" + productPrice + " (" + orderProduct.Quantity + " items at $" + orderProduct.Price + " each)");
-                }
+                    var productPrice = 0m;
+                    orderSummary.Append(product.ProductName);
  
-                orderSummary.Append("\r\n");
+                    if (product.PricingMethod == PerPound)
+                    {
+                        productPrice = (product.Weight.Value * product.Price);
+                        totalPrice += productPrice;
+                        orderSummary.Append($" {productPrice} ( {product.Weight} pounds at {product.Price} per pound)");
+                    }
+                    if(product.PricingMethod == PerItem)
+                    {
+                        productPrice = (product.Quantity.Value * product.Price);
+                        totalPrice += productPrice;
+                        orderSummary.Append($" {productPrice} ( {product.Quantity} items at {product.Price} each)");
+                    }
+ 
+                    orderSummary.Append("\r\n");
+                }
+                
+                Console.WriteLine(orderSummary.ToString());
+                Console.WriteLine($"Total Price: $ {totalPrice} \r\n");
             }
             
-            Console.WriteLine(orderSummary.ToString());
-            Console.WriteLine("Total Price: $" + price);
+          
  
             Console.ReadKey();
         }
-    }
- 
-    public class Product
-    {
-        public string ProductName;
-        public decimal Price;
-        public decimal? Weight;
-        public int? Quantity;
-        public string PricingMethod;
     }
 }
 
